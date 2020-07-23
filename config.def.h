cd18bcb368dd084547620c7dbb5d0286cdf0aea0
DedSecer <z2465216809qq.com@icloud.com>
2020-07-24 05:05:41 +0800

    add colorscheme

--- config.def.h
+++ config.def.h
@@ -100,45 +100,7 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 float alpha = 0.82;
 
-/* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
-};
-
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+#include "./colorscheme/default.h"
 
 /*
  * Default shape of cursor
@@ -155,27 +117,6 @@ static unsigned int cursorshape = 2;
 
 static unsigned int cols = 80;
 static unsigned int rows = 24;
-
-/*
- * Default colour and shape of the mouse cursor
- */
-static unsigned int mouseshape = XC_xterm;
-static unsigned int mousefg = 7;
-static unsigned int mousebg = 0;
-
-/*
- * Color used to display font attributes when fontconfig selected a font which
- * doesn't match the ones requested.
- */
-static unsigned int defaultattr = 11;
-/// Colors for the entities that are 'highlighted' in normal mode (search
-/// results currently on screen) [Vim Browse].
-static unsigned int highlightBg = 160;
-static unsigned int highlightFg = 15;
-/// Colors for highlighting the current cursor position (row + col) in normal
-/// mode [Vim Browse].
-static unsigned int currentBg = 8;
-static unsigned int currentFg = 15;
 
 /*
  * Force mouse select/shortcuts while mask is active (when MODE_MOUSE is set).
